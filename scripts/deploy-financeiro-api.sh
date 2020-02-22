2362090dab1576970050b7d2ebeedf445c762aaf
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 10:03:41 -0300

    Fiz script

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
 cd /home/ec2-user/projetos/financeiro-api
 mvn clean install
-sudo java -jar /home/ec2-user/projetos/financeiro-api/target/financeiro-api-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/projetos/financeiro-api/target/mfinanceiro-api-0.0.1-SNAPSHOT.jar

abe2087d3e66d545b23f033037f2fcd4b58aec5a
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 13:54:12 -0300

    Remove start script

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -2,4 +2,4 @@ ps -ef | grep financeiro-api | grep -v grep | awk '{print $2}' | xargs sudo kill
 sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
 cd /home/ec2-user/projetos/financeiro-api
 mvn clean install
-screen -dm -S FINANCEIRO-API start-financeiro-api.sh
+screen -dm -S FINANCEIRO-API java -jar /home/ec2-user/projetos/financeiro-api/target/financeiro-api-0.0.1-SNAPSHOT.jar
--- scripts/start-financeiro-api.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar /home/ec2-user/projetos/financeiro-api/target/financeiro-api-0.0.1-SNAPSHOT.jar

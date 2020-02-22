31d03b22fc2b02f96594bce228dd480a684c83c4
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 14:40:46 -0300

    remove stop script

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -1,4 +1,4 @@
-ps -ef | grep financeiro-api | grep -v grep | awk '{print $2}' | xargs sudo kill
+killall -9 financeiro-api
 sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
 cd /home/ec2-user/projetos/financeiro-api
 mvn clean install
--- scripts/stop-financeiro-api.sh
+++
@@ -1,1 +0,0 @@
-ps -ef | grep financeiro-api | grep -v grep | awk '{print $2}' | xargs sudo kill

fbb95273d8843a8dfd6147fe25d763136ea6f3a3
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 19:30:31 -0300

    change mvn command

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -1,5 +1,5 @@
 sudo -b service financeiro-api stop
 cd /home/ec2-user/projetos/financeiro-api
-mvn clean install
+mvn clean package
 sudo chmod 500 /home/ec2-user/projetos/financeiro-api/target/financeiro-api-0.0.1-SNAPSHOT.jar
 sudo -b service financeiro-api start

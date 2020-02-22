8efe18bfe83f074baf663765d22cd0785d382545
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 12:55:47 -0300

    Call script inside screen

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
-cd /home/ec2-user/projetos/financeiro-api
-mvn clean install
-java -jar /home/ec2-user/projetos/financeiro-api/target/financeiro-api-0.0.1-SNAPSHOT.jar
+screen -dm -S FINANCEIRO-API script-financeiro-api.sh

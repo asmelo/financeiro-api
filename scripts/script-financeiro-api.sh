0fd178046bd0d6cebcebafbe2627afc59078729d
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 13:20:48 -0300

    remove script of stopping

--- scripts/script-financeiro-api.sh
+++ scripts/script-financeiro-api.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ps -ef | grep financeiro-api | grep -v grep | awk '{print $2}' | xargs sudo kill
 sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
 cd /home/ec2-user/projetos/financeiro-api
 mvn clean install
--- scripts/stop-financeiro-api.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps -ef | grep financeiro-api | grep -v grep | awk '{print $2}' | xargs sudo kill

5c68e771eb68de4a80f6a4b97085ba2d46d12717
Alexandre Melo <asmelo10@hotmail.com>
2020-02-22 10:09:43 -0300

    fix script

--- scripts/deploy-financeiro-api.sh
+++ scripts/deploy-financeiro-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo chown -R ec2-user:ec2-user /home/ec2-user/projetos/financeiro-api
 cd /home/ec2-user/projetos/financeiro-api
-mvn clean install
-java -jar /home/ec2-user/projetos/financeiro-api/target/mfinanceiro-api-0.0.1-SNAPSHOT.jar
+sudo mvn clean install
+sudo java -jar /home/ec2-user/projetos/financeiro-api/target/mfinanceiro-api-0.0.1-SNAPSHOT.jar

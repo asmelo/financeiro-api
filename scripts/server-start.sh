3c8b2f2660ddbb26308e675e783f413e3e7722e8
Alexandre Melo <asmelo10@hotmail.com>
2020-02-24 19:59:34 -0300

    Change folder on server-start script

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/projetos/financeiro-api
+cd /home/ec2-user/projetos/financeiro-api/target
 sudo java -jar -Dserver.port=8080 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
